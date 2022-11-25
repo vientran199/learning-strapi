7846662172bda304174bd6fc343b8d82505efeab
vientran199 <vientran1999@gmail.com>
2022-11-25 14:21:39 +0700

    Setup import export entries

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,20 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  // 'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          // Enable the download of the Monaco editor
+          // from cdn.jsdelivr.net.
+          "script-src": ["'self'", "cdn.jsdelivr.net", "blob:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   // {
   //   name: 'strapi::cors',
@@ -13,7 +27,13 @@ module.exports = [
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
-  'strapi::body',
+  // 'strapi::body',
+  {
+    name: 'strapi::body',
+    config: {
+      jsonLimit: '10mb', // Giới hạn khi import
+    },
+  },
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
--- config/plugins.js
+++ config/plugins.js
@@ -15,5 +15,17 @@ module.exports = {
       enabled: true,
       resolve: './src/plugins/todo'
     },
+    'import-export-entries': {
+      enabled: true,
+      config: {
+        /**
+         * Public hostname of the server.
+         *
+         * If you use the local provider to persist medias,
+         * `serverPublicHostname` should be set to properly export media urls.
+         */
+        serverPublicHostname: 'https://yoga.com', // default: "".
+      },
+    },
     // ...
   }
